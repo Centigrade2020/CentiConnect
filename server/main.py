7a94b0310f934e0436fd2b48e20f44b1394a71d4
Dharun Sivakumar <72387676+dharunvs@users.noreply.github.com>
2021-06-06 17:47:15 +0530

    Updates

--- server/main.py
+++ server/main.py
@@ -1,13 +1,20 @@
+import pyrebase
+
 from flask import Flask, jsonify
+from flask_marshmallow import Marshmallow
+from services import firebase as fb
 
 app = Flask(__name__)
+
+ma = Marshmallow(app)
 
 @app.route("/get", methods=['GET'])
 def home():
     return jsonify({
-        "Hello": "World"
+        "Hello": "World",
     })
 
 
 if __name__ == "__main__":
+    print(Marshmallow)
     app.run(debug=True)
