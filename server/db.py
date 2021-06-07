7e732e05a5ccc42a5c3261737ec1ed1d78dc0e01
Hrithik <hrithikmj003@gmail.com>
2021-06-07 19:05:11 +0530

    update

---
+++ server/db.py
@@ -0,0 +1,26 @@
+from services import firebase as fb
+
+
+def new_post(uid):
+    pass
+    '''
+    {
+        "uid":"xxxx",
+        "pic_id":"yyyy",
+        "description":"",
+        "interactions":"",
+    }
+    '''
+
+
+def remove_post(uid):
+    # deleting the json and image from storage
+    pass
+
+
+def get_user(uid):
+    pass
+
+
+if __name__ == "__main__":
+    print("hello")
