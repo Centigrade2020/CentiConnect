e80b35e441f732931302533b913a518d7d909890
Dharun Sivakumar <72387676+dharunvs@users.noreply.github.com>
2021-06-07 09:44:19 +0530

    Updates

--- server/main.py
+++ server/main.py
@@ -5,7 +5,7 @@ from services import firebase as fb
 app = Flask(__name__)
 
 
-def Signup(email, password, username):
+def create_user(email, password, username):
     try:
         user = fb.auth.create_user(email=email, password=password)
         fb.firestore.collection("users").document(user.uid).set({
@@ -15,29 +15,14 @@ def Signup(email, password, username):
     except BaseException as e:
         return({"error": f"{e.code}"})
 
-def Login(email, password):
-    user = fb.auth.get_user_by_email(email)
-    user = fb.auth.get_user(user.uid)
-    print(user.password)
-
 
 @app.route("/signup", methods=["GET", "POST"])
 def signup():
     
     if request.method == "POST":
         content = request.get_json()
-        data = signup()(content["email"], content["password"], content["username"])
+        data = create_user(content["email"], content["password"], content["username"])
         return jsonify(data)
-    else:
-        return {}
-
-@app.route("/login", methods=["GET", "POST"])
-def login():
-    
-    if request.method == "POST":
-        content = request.get_json()
-        Login(content["email"], content["password"])
-        return {}
     else:
         return {}
 
