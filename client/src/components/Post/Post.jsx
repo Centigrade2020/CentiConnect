d2a36996d9e87bea14fce4884f1e7683bc7f606a
Dharun Sivakumar <72387676+dharunvs@users.noreply.github.com>
2021-06-08 10:42:11 +0530

    Updates

--- client/src/components/Post/Post.jsx
+++ client/src/components/Post/Post.jsx
@@ -1,35 +1,43 @@
 import { Symbols } from "../../components";
 import "./Post.css";
-import test from "./test.jpg";
 
-const Post = () => {
+const Post = ({
+  postId,
+  comments,
+  username,
+  image,
+  upvotes,
+  downvotes,
+  caption,
+}) => {
   return (
-    <div className="Post">
+    <div className="Post" key={postId}>
       <div className="profile">
         <div className="profileIcon"></div>
-        <p className="profileName">legend</p>
+        <p className="profileName">{username}</p>
         <div className="settings">
           <Symbols.ThreeDots size="40" />
         </div>
       </div>
       <section className="imageSection">
         <div className="imageContainer">
-          <img src={test} alt="image" />
+          <img src={image} alt="image" />
         </div>
       </section>
       <section className="commentSection">
         <div className="comments">
+          {Object.keys(comments).map((i) => {
+            console.log(i);
+            return (
+              <div className="comment" key={i}>
+                <h4>{i}</h4>
+                <p>{comments[`${i}`]}</p>
+              </div>
+            );
+          })}
           <div className="comment">
             <h4>senpai</h4>
             <p>Looking gud broo!!</p>
-          </div>
-          <div className="comment">
-            <h4>dharundds</h4>
-            <p>Wow.. Awesome!!</p>
-          </div>
-          <div className="comment">
-            <h4>hrithik</h4>
-            <p>waste ra dei!!</p>
           </div>
         </div>
 
@@ -37,17 +45,20 @@ const Post = () => {
           <div className="postLinks">
             <div className="upVote">
               <Symbols.UpVote size="20" />
-              24
+              {upvotes}
             </div>
             <div className="downVote">
-              <Symbols.DownVote size="20" />3
+              <Symbols.DownVote size="20" />
+              {downvotes}
             </div>
           </div>
           <div className="descriptionContent">
             <p>
-              <span className="profileName">legend</span>Hello frands.. Diwali
+              <span className="profileName">{username}</span>
+              {caption}
+              {/* Hello frands.. Diwali
               outfit Hello frands.. Diwali outfit Hello frands.. Diwali outfit
-              Hello frands.. Diwali outfit v Hello frands.. Diwali outfit
+              Hello frands.. Diwali outfit v Hello frands.. Diwali outfit */}
             </p>
           </div>
         </div>
--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,50 +1,73 @@
 import { Symbols, Post } from "../../components";
 import "./Home.css";
+import test from "./test.jpg";
 
 function Home() {
-  const create_post = async()=>{
-    const content= {
-      username:'1234',
-      imageId:'9089',
-      content:'hello',
-      description:'nice one buddy',
+  const create_post = async () => {
+    const content = {
+      username: "1234",
+      imageId: "9089",
+      content: "hello",
+      description: "nice one buddy",
     };
-  
-    await fetch('/createpost',{
-      method:'POST',
-      headers:{
+
+    await fetch("/createpost", {
+      method: "POST",
+      headers: {
         "Content-Type": "application/json",
       },
-      body:JSON.stringify(content),
-    }) .then((res) => {
+      body: JSON.stringify(content),
+    }).then((res) => {
       return res.json();
-    }) 
-  }
+    });
+  };
 
-  const delete_post = async()=>{
-    const imageId={
-      imageId:'9089',
-    }
-    await fetch('/deletepost',{
-      method:"POST",
-      headers:{
-        "Content-Type":"application/json",
+  const delete_post = async () => {
+    const imageId = {
+      imageId: "9089",
+    };
+    await fetch("/deletepost", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
       },
       body: JSON.stringify(imageId),
-    })
-  }
+    });
+  };
+
+  const post = {
+    postId: "asdf",
+    comments: {
+      senpai: "Looking gud broo!!",
+      dharundds: "Wow.. Awesome!!",
+      hrithik: "waste ra dei!!",
+    },
+    username: "legend",
+    image: test,
+    upvotes: 24,
+    downvotes: 3,
+    caption:
+      "Hello frands.. Diwali outfit Hello frands.. Diwali outfit Hello frands.. Diwali outfit Hello frands.. Diwali outfit v Hello frands.. Diwali outfit",
+  };
 
   return (
     <div className="Home">
       <div className="posts">
-        <Post />
-        <Post />
+        <Post
+          postId={post.postId}
+          comments={post.comments}
+          username={post.username}
+          image={post.image}
+          upvotes={post.username}
+          downvotes={post.downvotes}
+          caption={post.caption}
+        />
       </div>
       <div className="createPostButton">
         <Symbols.Plus size="38" />
       </div>
       <button onClick={create_post}>click</button>
-  <button onClick={delete_post}>delete</button>
+      <button onClick={delete_post}>delete</button>
     </div>
   );
 }
