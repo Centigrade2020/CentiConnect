11d5b0d734cbc990e70158528e7c37ffe4d65908
Hrithik <hrithikmj003@gmail.com>
2021-06-09 14:33:25 +0530

    minor corrections

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -8,15 +8,17 @@ function NavBar() {
     <div className="NavBar">
       <div className="navBarContainer">
         <div className="logo">
-          <h1>Connect</h1>
+          <h1 onClick={() => {
+            history.push('')
+          }} > Connect</h1>
         </div>
         <div className="searchContainer">
           <SearchBar />
         </div>
         <div className="profileContainer">
-          <p className="profileName" onClick={()=>{
-          history.push('profile')
-        }}>username</p>
+          <p className="profileName" onClick={() => {
+            history.push('profile')
+          }}>username</p>
           <img
             className="profilePicContainer"
             onClick={() => {
