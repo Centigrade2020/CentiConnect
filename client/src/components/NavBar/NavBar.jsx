ee7896a0d93f3ee3fb1c164b1104620262dec176
dharun <dharundds@gmail.com>
2021-06-10 08:58:51 +0530

    Update NavBar.jsx

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -1,9 +1,23 @@
 import SearchBar from "../SearchBar/SearchBar";
 import "./NavBar.css";
 import { useHistory } from "react-router";
-
+import {useEffect,useState} from "react";
+import fb from "../../services/firebase"
 function NavBar() {
   const history = useHistory();
+  const [username,setusername] = useState("")
+  useEffect(() => {
+    if (!!localStorage.getItem("userId")) {
+      const uid = localStorage.getItem("userId");
+      var docRef = fb.firestore.collection("users").doc(uid);
+      
+      docRef.get().then((doc) => {
+        setusername(doc.data().username)
+        
+      });
+    }
+  }, []);
+ 
 
   return (
     <div className="NavBar">
@@ -26,7 +40,7 @@ function NavBar() {
             history.push("profile");
           }}
         >
-          <p className="profileName">username</p>
+          <p className="profileName">{username}</p>
           <div className="profilePicContainer">
             <img
               src="https://firebasestorage.googleapis.com/v0/b/centiconnect.appspot.com/o/postImages%2F63cd11a998da4ef38ca4967fdf64c88a.jpeg?alt=media&token=2ae5afcd-2e23-4a98-a98d-487980654099"
