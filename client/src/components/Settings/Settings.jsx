94f2de22238f9e51c3ef83bbdbd7adcde362ab68
Dharun Sivakumar <72387676+dharunvs@users.noreply.github.com>
2021-06-11 16:48:18 +0530

    Updates

--- client/src/components/Settings/Settings.jsx
+++ client/src/components/Settings/Settings.jsx
@@ -1,7 +1,18 @@
 import "./Settings.css";
 
 function Settings() {
-  return <div className="Settings">Settings</div>;
+  return (
+    <div className="Settings">
+      <div className="settingOptionContainer">
+        <ul>
+          <li>Profile</li>
+          <li>Change email</li>
+          <li>Change password</li>
+          <li></li>
+        </ul>
+      </div>
+    </div>
+  );
 }
 
 export default Settings;
